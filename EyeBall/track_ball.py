7ea9dc0f13b488c78f58230b099d4523bc8631d5
Sameer <Sameer@MacBook-Air-4.local>
2025-04-07 00:09:39 -0400

    Separate kalman filter class
    Signed-off-by: Sameer <Sameer@MacBook-Air-4.local>

--- EyeBall/track_ball.py
+++ EyeBall/track_ball.py
@@ -1,287 +1,42 @@
 # https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
-# import the necessary packages
-from collections import deque # maintain a list of the past N (x, y)-locations of the ball in our video stream. Maintaining such a queue allows us to draw the “contrail” of the ball as its being tracked.
-from imutils.video import VideoStream
+
+from collections import deque
 import numpy as np
 import cv2
 import imutils
 import time
 from scipy.optimize import linear_sum_assignment
+from KalmanFilter import KalmanFilter, Track, Tracker
+
 
 # define the lower and upper boundaries of the "green" ball in the HSV color space, then initialize the list of tracked points
 # ball_steph2: (10, 174, 138), low = (8, 160, 130), high = (12, 180, 150)
 # ball_jordan3 = (7, 154, 86) low = (6,145,75), high = (9, 165, 95) https://imagecolorpicker.com/, https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html
-ballColorLower = (40,130,100)  #(8,150,100) # for 2ktest #(8,150,110) #(3,100,110) # (3,140,75)#(5,140,75)
-ballColorUpper = (100,200,200) #(14,255,255) # for 2ktest #(14,255,255)#(10, 170, 95)
-lower_area = 350#20
-upper_area = 700#200
+ballColorLower = (20,50,110) # (40,130,100) # for 2ktest #(8,150,110) #(3,100,110) # (3,140,75)#(5,140,75)
+ballColorUpper = (50,90,160) # (100,200,200) # for 2ktest #(14,255,255)#(10, 170, 95)
+ballColorLower_BGR = np.uint8([[list((20, 50, 110))]])  # Lower
+ballColorUpper_BGR = np.uint8([[list((90, 255, 255))]])  # Upper
+
+# Convert to HSV
+ballColorLower_HSV = cv2.cvtColor(ballColorLower_BGR, cv2.COLOR_BGR2HSV)[0][0]
+ballColorUpper_HSV = cv2.cvtColor(ballColorUpper_BGR, cv2.COLOR_BGR2HSV)[0][0]
+lower_area = 20 # 350#20
+upper_area = 200 # 700#200
 dist = 150
 frames = 40
 trace = 3500
 
-pts = deque(maxlen=64)
+pts = deque(maxlen=64)  # maintain a list of the past N (x, y)-locations of the ball in our video stream. Maintaining such a queue allows us to draw the “contrail” of the ball as its being tracked.
 
 play_name = 'fist21'
-video = "/Users/Sameer/Documents/Coding/EyeBall-v2/5_cropped.mp4"#spurs_play_'+play_name+'.mp4'
+video = '/Users/Sameer/Documents/Coding/Muse/EyeBall/videos/spurs_play_'+play_name+'.mp4'
 vs = cv2.VideoCapture(video)
 hasFrame, frame = vs.read()
-vid_writer = cv2.VideoWriter('5_detected.mp4', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame.shape[1], frame.shape[0]))
+output_path = '/Users/Sameer/Documents/Coding/Muse/EyeBall/videos/output'+play_name+'_output.mp4'
+vid_writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame.shape[1], frame.shape[0]))
 
 # allow the camera or video file to warm up
 time.sleep(2.0)
-
-
-"""uses code from https://github.com/srianant/kalman_filter_multi_object_tracking"""
-class Track(object):
-    """Track class for every object to be tracked
-    Attributes:
-        None
-    """
-
-    def __init__(self, prediction, trackIdCount):
-        """Initialize variables used by Track class
-        Args:
-            prediction: predicted centroids of object to be tracked
-            trackIdCount: identification of each track object
-        Return:
-            None
-        """
-        self.track_id = trackIdCount  # identification of each track object
-        self.KF = KalmanFilter()  # KF instance to track this object
-        self.prediction = np.asarray(prediction)  # predicted centroids (x,y)
-        self.skipped_frames = 0  # number of frames skipped undetected
-        self.trace = []  # trace path
-
-
-class Tracker(object):
-    """Tracker class that updates track vectors of object tracked
-    Attributes:
-        None
-    """
-
-    def __init__(self, dist_thresh, max_frames_to_skip, max_trace_length,
-                 trackIdCount):
-        """Initialize variable used by Tracker class
-        Args:
-            dist_thresh: distance threshold. When exceeds the threshold,
-                         track will be deleted and new track is created
-            max_frames_to_skip: maximum allowed frames to be skipped for
-                                the track object undetected
-            max_trace_lenght: trace path history length
-            trackIdCount: identification of each track object
-        Return:
-            None
-        """
-        self.dist_thresh = dist_thresh
-        self.max_frames_to_skip = max_frames_to_skip
-        self.max_trace_length = max_trace_length
-        self.tracks = []
-        self.trackIdCount = trackIdCount
-
-    def Update(self, detections):
-        """Update tracks vector using following steps:
-            - Create tracks if no tracks vector found
-            - Calculate cost using sum of square distance
-              between predicted vs detected centroids
-            - Using Hungarian Algorithm assign the correct
-              detected measurements to predicted tracks
-              https://en.wikipedia.org/wiki/Hungarian_algorithm
-            - Identify tracks with no assignment, if any
-            - If tracks are not detected for long time, remove them
-            - Now look for un_assigned detects
-            - Start new tracks
-            - Update KalmanFilter state, lastResults and tracks trace
-        Args:
-            detections: detected centroids of object to be tracked
-        Return:
-            None
-        """
-
-        # Create tracks if no tracks vector found
-        if (len(self.tracks) == 0):
-            for i in range(len(detections)):
-                track = Track(detections[i], self.trackIdCount)
-                self.trackIdCount += 1
-                self.tracks.append(track)
-
-        # Calculate cost using sum of square distance between
-        # predicted vs detected centroids
-        N = len(self.tracks)
-        M = len(detections)
-        cost = np.zeros(shape=(N, M))   # Cost matrix
-        for i in range(len(self.tracks)):
-            for j in range(len(detections)):
-                try:
-                    diff = self.tracks[i].prediction - detections[j]
-                    distance = np.sqrt(diff[0][0]*diff[0][0] +
-                                       diff[1][0]*diff[1][0])
-                    cost[i][j] = distance
-                except:
-                    pass
-
-        # Let's average the squared ERROR
-        cost = (0.5) * cost
-        # Using Hungarian Algorithm assign the correct detected measurements
-        # to predicted tracks
-        assignment = []
-        for _ in range(N):
-            assignment.append(-1)
-        row_ind, col_ind = linear_sum_assignment(cost)
-        for i in range(len(row_ind)):
-            assignment[row_ind[i]] = col_ind[i]
-
-        # Identify tracks with no assignment, if any
-        un_assigned_tracks = []
-        for i in range(len(assignment)):
-            if (assignment[i] != -1):
-                # check for cost distance threshold.
-                # If cost is very high then un_assign (delete) the track
-                if (cost[i][assignment[i]] > self.dist_thresh):
-                    assignment[i] = -1
-                    un_assigned_tracks.append(i)
-                pass
-            else:
-                self.tracks[i].skipped_frames += 1
-
-        # If tracks are not detected for long time, remove them
-        del_tracks = []
-        for i in range(len(self.tracks)):
-            if (self.tracks[i].skipped_frames > self.max_frames_to_skip):
-                del_tracks.append(i)
-        if len(del_tracks) > 0:  # only when skipped frame exceeds max
-            for id in del_tracks:
-                if id < len(self.tracks):
-                    del self.tracks[id]
-                    del assignment[id]
-                else:
-                    print("ERROR: id is greater than length of tracks")
-
-        # Now look for un_assigned detects
-        un_assigned_detects = []
-        for i in range(len(detections)):
-                if i not in assignment:
-                    un_assigned_detects.append(i)
-
-        # Start new tracks
-        if(len(un_assigned_detects) != 0):
-            for i in range(len(un_assigned_detects)):
-                track = Track(detections[un_assigned_detects[i]],
-                              self.trackIdCount)
-                self.trackIdCount += 1
-                self.tracks.append(track)
-
-        # Update KalmanFilter state, lastResults and tracks trace
-        for i in range(len(assignment)):
-            self.tracks[i].KF.predict()
-
-            if(assignment[i] != -1):
-                self.tracks[i].skipped_frames = 0
-                self.tracks[i].prediction = self.tracks[i].KF.correct(
-                                            detections[assignment[i]], 1)
-            else:
-                self.tracks[i].prediction = self.tracks[i].KF.correct(
-                                            np.array([[0], [0]]), 0)
-
-            if(len(self.tracks[i].trace) > self.max_trace_length):
-                for j in range(len(self.tracks[i].trace) -
-                               self.max_trace_length):
-                    del self.tracks[i].trace[j]
-            #print(self.tracks[i].prediction)
-            self.tracks[i].trace.append(self.tracks[i].prediction)
-            self.tracks[i].KF.lastResult = self.tracks[i].prediction
-
-class KalmanFilter(object):
-    """Kalman Filter class keeps track of the estimated state of
-    the system and the variance or uncertainty of the estimate.
-    Predict and Correct methods implement the functionality
-    Reference: https://en.wikipedia.org/wiki/Kalman_filter
-    Attributes: None
-    """
-
-    def __init__(self):
-        """Initialize variable used by Kalman Filter class
-        Args:
-            None
-        Return:
-            None
-        """
-        self.dt = 0.005  # delta time
-
-        self.A = np.array([[1, 0], [0, 1]])  # matrix in observation equations
-        self.u = np.zeros((2, 1))  # previous state vector
-
-        # (x,y) tracking object center
-        self.b = np.array([[0], [255]])  # vector of observations
-
-        self.P = np.diag((3.0, 3.0))  # covariance matrix
-        self.F = np.array([[1.0, self.dt], [0.0, 1.0]])  # state transition mat
-
-        self.Q = np.eye(self.u.shape[0])  # process noise matrix
-        self.R = np.eye(self.b.shape[0])  # observation noise matrix
-        self.lastResult = np.array([[0], [255]])
-
-    def predict(self):
-        """Predict state vector u and variance of uncertainty P (covariance).
-            where,
-            u: previous state vector
-            P: previous covariance matrix
-            F: state transition matrix
-            Q: process noise matrix
-        Equations:
-            u'_{k|k-1} = Fu'_{k-1|k-1}
-            P_{k|k-1} = FP_{k-1|k-1} F.T + Q
-            where,
-                F.T is F transpose
-        Args:
-            None
-        Return:
-            vector of predicted state estimate
-        """
-        # Predicted state estimate
-        self.u = np.round(np.dot(self.F, self.u))
-        # Predicted estimate covariance
-        self.P = np.dot(self.F, np.dot(self.P, self.F.T)) + self.Q
-        self.lastResult = self.u  # same last predicted result
-        return self.u
-
-    def correct(self, b, flag):
-        """Correct or update state vector u and variance of uncertainty P (covariance).
-        where,
-        u: predicted state vector u
-        A: matrix in observation equations
-        b: vector of observations
-        P: predicted covariance matrix
-        Q: process noise matrix
-        R: observation noise matrix
-        Equations:
-            C = AP_{k|k-1} A.T + R
-            K_{k} = P_{k|k-1} A.T(C.Inv)
-            u'_{k|k} = u'_{k|k-1} + K_{k}(b_{k} - Au'_{k|k-1})
-            P_{k|k} = P_{k|k-1} - K_{k}(CK.T)
-            where,
-                A.T is A transpose
-                C.Inv is C inverse
-        Args:
-            b: vector of observations
-            flag: if "true" prediction result will be updated else detection
-        Return:
-            predicted state vector u
-        """
-
-        if not flag:  # update using prediction
-            self.b = self.lastResult
-        else:  # update using detection
-            self.b = b
-        C = np.dot(self.A, np.dot(self.P, self.A.T)) + self.R
-        K = np.dot(self.P, np.dot(self.A.T, np.linalg.inv(C)))
-
-        self.u = np.round(self.u + np.dot(K, (self.b - np.dot(self.A,
-                                                              self.u))))
-        self.P = self.P - np.dot(K, np.dot(C, K.T))
-        self.lastResult = self.u
-        return self.u
-
-
 
 tracker = Tracker(dist, frames, trace, 1)
 
@@ -307,11 +62,11 @@ while True:
     blurred = cv2.GaussianBlur(frame, (11, 11), 0) # reduce high frequency noise and allow us to focus on the structural objects
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     # cv2.imshow("blurred",blurred)
-    # cv2.imshow("hsv",hsv)
+    cv2.imshow("hsv",hsv)
     # construct a mask for the color, then perform a series of dilations and erosions to remove any small blobs left in the mask
 
     element = None #np.ones((5,5)).astype(np.uint8) # element for morphology, default None
-    mask = cv2.inRange(hsv, ballColorLower, ballColorUpper) # handles the actual localization of the ball
+    mask = cv2.inRange(hsv, ballColorLower_HSV, ballColorUpper_HSV) # handles the actual localization of the ball
     mask = cv2.erode(mask, element, iterations=2) # erode and dilate to remove small blobs
     mask = cv2.dilate(mask, element, iterations=2)
     cv2.imshow("mask",mask)
@@ -465,11 +220,11 @@ while True:
     vid_writer.write(frame) #writes to a video
     cv2.imshow("Frame", frame)
     cv2.waitKey()
-    #key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1) & 0xFF
     startCount = startCount-1
     # if the 'q' key is pressed, stop the loop
-    #if key == ord("q"):
-    #	break
+    if key == ord("q"):
+    	break
 
 vid_writer.release()
 cv2.destroyAllWindows()
